fb55a2be1706a9a5975802bb98dbda3da045b12f
dvitale199 <d.vitale199@gmail.com>
2022-01-11 16:00:23 -0500

    first commit

---
+++ clusterbuster.py
@@ -0,0 +1,30 @@
+# imports
+import pandas as pd
+
+
+def read_report():
+    pass
+
+def flag_variant():
+    pass
+
+def search_variant():
+    pass
+
+def variant_report():
+    pass
+
+def recluster_variant():
+    pass
+
+def export_cluster():
+    pass
+
+def export_final_report():
+    pass
+
+
+
+
+
+
