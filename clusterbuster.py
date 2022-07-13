bfb1b1152af03cac70978b0349840d868a557128
dvitale199 <d.vitale199@gmail.com>
2022-07-13 19:15:01 +0000

    updated calculate_maf() and parse_report() methods to handle new data format

--- clusterbuster.py
+++ clusterbuster.py
@@ -29,13 +29,14 @@ def calculate_maf(gtype_df):
         '''
     
     gtypes_map = {
-        'AA':0,
-        'AB':1,
-        'BA':1,
-        'BB':2,
-        'NC':np.nan
+        'AA': 0,
+        'AB': 1,
+        'BA': 1,
+        'BB': 2,
+        'NC': np.nan
         }
-    gtypes = gtype_df.set_index('snpid').replace(gtypes_map).copy()
+
+    gtypes = gtype_df.pivot(index='snpID', columns='Sample_ID', values='GType').replace(gtypes_map)
     
     # count only called genotypes
     N = gtypes.shape[1]-gtypes.isna().sum(axis=1)
@@ -45,74 +46,90 @@ def calculate_maf(gtype_df):
 
     return maf_out
 
-# @st.cache()
 def parse_report(report_in, flag_maf, flag_gentrain):
 
-    ''' 
-    in: 
-        reportfile path
-    out: 
-        dict {
-            report: variant     theta     r 
-            flagged_vars: variant   maf     gt_score    flag
-            }
+    gtype_df = report_in
+    maf_df = report_in.loc[:,['Sample_ID','snpID','GType']] 
+    maf_scores_df = calculate_maf(maf_df)
+    out_df = maf_scores_df.merge(gtype_df, how='inner', on='snpID')
+    out_df.loc[:,'maf_flag'] = np.where(out_df.maf<flag_maf, True, False)
+    out_df.loc[:,'gentrain_flag'] = np.where(out_df.GenTrain_Score<flag_gentrain, True, False)
+
+    return out_df
+
+
+
+  
+
+
+######### old method to be removed 
+# def parse_report(report_in, flag_maf, flag_gentrain):
+
+#     ''' 
+#     in: 
+#         reportfile path
+#     out: 
+#         dict {
+#             report: variant     theta     r 
+#             flagged_vars: variant   maf     gt_score    flag
+#             }
             
-    '''
+#     '''
     
-    report = report_in.drop(columns=['Index', 'Address', 'Chr', 'Position', 'GenTrain Score', 'Frac A', 'Frac C', 'Frac G', 'Frac T'])
+#     report = report_in.drop(columns=['Index', 'Address', 'Chr', 'Position', 'GenTrain Score', 'Frac A', 'Frac C', 'Frac G', 'Frac T'])
 
-    # Chop out to GType, Score and Theta dataframes.
-    Name_df = report[['Name']].copy()
-    GType_df = report.loc[:, report.columns.str.endswith(".GType")]
-    Theta_df = report.loc[:, report.columns.str.endswith(".Theta")]
-    R_df = report.loc[:, report.columns.str.endswith(".R")]
+#     # Chop out to GType, Score and Theta dataframes.
+#     Name_df = report[['Name']].copy()
+#     GType_df = report.loc[:, report.columns.str.endswith(".GType")]
+#     Theta_df = report.loc[:, report.columns.str.endswith(".Theta")]
+#     R_df = report.loc[:, report.columns.str.endswith(".R")]
 
-    Name_df['Name.GType'] = Name_df.loc[:,'Name'] + ".GType"
-    Name_df['Name.Theta'] = Name_df.loc[:,'Name'] + ".Theta"
-    Name_df['Name.R'] = Name_df.loc[:,'Name'] + ".R"
+#     Name_df['Name.GType'] = Name_df.loc[:,'Name'] + ".GType"
+#     Name_df['Name.Theta'] = Name_df.loc[:,'Name'] + ".Theta"
+#     Name_df['Name.R'] = Name_df.loc[:,'Name'] + ".R"
 
-    # Merge names plus data types.
-    Name_GType_df = pd.concat([Name_df, GType_df], axis=1).rename(columns={"Name.GType":"idx"}).drop(columns=['Name', 'Name.Theta', 'Name.R'])
-    Name_GType_df.columns = Name_GType_df.columns.str.replace(".GType", "", regex=False)
-    Name_GType_df_final = Name_GType_df.set_index('idx')
+#     # Merge names plus data types.
+#     Name_GType_df = pd.concat([Name_df, GType_df], axis=1).rename(columns={"Name.GType":"idx"}).drop(columns=['Name', 'Name.Theta', 'Name.R'])
+#     Name_GType_df.columns = Name_GType_df.columns.str.replace(".GType", "", regex=False)
+#     Name_GType_df_final = Name_GType_df.set_index('idx')
 
-    Name_Theta_df = pd.concat([Name_df, Theta_df], axis=1).rename(columns={"Name.Theta": "idx"}).set_index('idx').drop(columns=['Name', 'Name.GType', 'Name.R'])
-    Name_Theta_df.columns = Name_Theta_df.columns.str.replace(".Theta", "", regex=False)
+#     Name_Theta_df = pd.concat([Name_df, Theta_df], axis=1).rename(columns={"Name.Theta": "idx"}).set_index('idx').drop(columns=['Name', 'Name.GType', 'Name.R'])
+#     Name_Theta_df.columns = Name_Theta_df.columns.str.replace(".Theta", "", regex=False)
 
-    Name_R_df = pd.concat([Name_df, R_df], axis=1).rename(columns={"Name.R":"idx"}).set_index('idx').drop(columns=['Name', 'Name.Theta', 'Name.GType'])
-    Name_R_df.columns = Name_R_df.columns.str.replace(".R", "", regex=False)
+#     Name_R_df = pd.concat([Name_df, R_df], axis=1).rename(columns={"Name.R":"idx"}).set_index('idx').drop(columns=['Name', 'Name.Theta', 'Name.GType'])
+#     Name_R_df.columns = Name_R_df.columns.str.replace(".R", "", regex=False)
 
-    #  Transpose the data frames and make the names of the variants plus the suffixes the columns.
-    GType_transposed_df = Name_GType_df_final.transpose()
-    Theta_transposed_df = Name_Theta_df.transpose()
-    R_transposed_df = Name_R_df.transpose()
+#     #  Transpose the data frames and make the names of the variants plus the suffixes the columns.
+#     GType_transposed_df = Name_GType_df_final.transpose()
+#     Theta_transposed_df = Name_Theta_df.transpose()
+#     R_transposed_df = Name_R_df.transpose()
 
-    # Smash everything together and get ready for plotting.
-    temp_df = GType_transposed_df.merge(Theta_transposed_df, left_index=True, right_index=True)
-    clusterbuster_df = temp_df.merge(R_transposed_df, left_index=True, right_index=True)
-    clusterbuster_out = clusterbuster_df.reset_index().rename(columns={'index':'IID'})
+#     # Smash everything together and get ready for plotting.
+#     temp_df = GType_transposed_df.merge(Theta_transposed_df, left_index=True, right_index=True)
+#     clusterbuster_df = temp_df.merge(R_transposed_df, left_index=True, right_index=True)
+#     clusterbuster_out = clusterbuster_df.reset_index().rename(columns={'index':'IID'})
     
-    # get gentrain scores
+#     # get gentrain scores
     
-    gtrain_scores_df = report_in.loc[:,['Name', 'Chr', 'Position','GenTrain Score','Frac A', 'Frac C', 'Frac G', 'Frac T']]
-    gtrain_scores_df.columns = ['snpid','Chr','Position','gentrain_score','Frac_A','Frac_C','Frac_G','Frac_T']
-    # calculate maf
-    gtype_df = Name_GType_df.copy()
-    gtype_df.loc[:,'snpid'] = gtype_df.loc[:,'idx'].str.replace(".GType", "", regex=False)
-    gtype_to_maf = gtype_df.drop(columns=['idx'])
-    maf_scores_df = calculate_maf(gtype_to_maf)
-    flag_df = maf_scores_df.merge(gtrain_scores_df, how='inner', on='snpid')
-    flag_df.loc[:,'maf_flag'] = np.where(flag_df.maf<flag_maf, True, False)
-    flag_df.loc[:,'gentrain_flag'] = np.where(flag_df.gentrain_score<flag_gentrain, True, False)
+#     gtrain_scores_df = report_in.loc[:,['Name', 'Chr', 'Position','GenTrain Score','Frac A', 'Frac C', 'Frac G', 'Frac T']]
+#     gtrain_scores_df.columns = ['snpid','Chr','Position','gentrain_score','Frac_A','Frac_C','Frac_G','Frac_T']
+#     # calculate maf
+#     gtype_df = Name_GType_df.copy()
+#     gtype_df.loc[:,'snpid'] = gtype_df.loc[:,'idx'].str.replace(".GType", "", regex=False)
+#     gtype_to_maf = gtype_df.drop(columns=['idx'])
+#     maf_scores_df = calculate_maf(gtype_to_maf)
+#     flag_df = maf_scores_df.merge(gtrain_scores_df, how='inner', on='snpid')
+#     flag_df.loc[:,'maf_flag'] = np.where(flag_df.maf<flag_maf, True, False)
+#     flag_df.loc[:,'gentrain_flag'] = np.where(flag_df.gentrain_score<flag_gentrain, True, False)
 
-    missing_df = pd.DataFrame()
+#     missing_df = pd.DataFrame()
 
-    out_dict = {
-        'clusterbuster_df': clusterbuster_out,
-        'flagged_snps': flag_df, 
-    }
+#     out_dict = {
+#         'clusterbuster_df': clusterbuster_out,
+#         'flagged_snps': flag_df, 
+#     }
 
-    return out_dict
+#     return out_dict
 
 
 def view_table_slice(df_in, max_rows=20, **st_dataframe_kwargs):
